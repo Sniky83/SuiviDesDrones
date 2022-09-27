50633a53aebaf9f43f12abdfde59a0a4ed73b4af
Sniky83 <sniky83@hotmail.fr>
2022-09-27 14:43:46 +0200

    useless files delete

--- PatternIoC/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using PatternIoC;
-
-Bus bus = new ();
-Velo velo = new ();
-VoitureElectrique voitureElectrique = new ();
-
-Person person = new("Igor", voitureElectrique);
-person.AllerAuTravail(new Destination("14 rue de saint michel"));
